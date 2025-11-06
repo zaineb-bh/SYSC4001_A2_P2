a943db0661744f1124f4fcf0f04c9060d7b17b1f
zaineb <zainebbenhmida@cmail.carleton.ca>
2025-11-06 10:43:52 -0500

    question 4

--- process1.c
+++ process1.c
@@ -3,15 +3,43 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
+struct sharedMemory {
+    int multiple; 
+    int counter;  
+};
 
 int main()
 {  
+    //size (in bytes) of shared memory object 
+    const int SIZE = 4096;  
+    //name of the shared memory object             
+    const char *name = "OS";      
+
+    //shared memory file descriptor
+    int shm_fd;
+    // pointer to shared memory obect                      
+    struct sharedMemory *shared;      
+
+    // create the shared memory object
+    shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+
+    // configure the size of the shared memory object
+    ftruncate(shm_fd, SIZE);
+
+    // memory map the shared memory object
+    shared = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    shared->multiple = 3;
+    shared->counter = 0;
+    
     pid_t pid;
-    pid_t pid1;
     /* fork a child process */
     pid = fork();
-    int status;
 
     if (pid < 0) { /* error occurred */
         fprintf(stderr, "Fork Failed");
@@ -25,25 +53,27 @@ int main()
     }    
 
     else { /* parent process */
-        int counter = 0;
-        pid1 = getpid();
 
-        while(1){
-            if (waitpid(pid, &status, WNOHANG) != 0) {
-                printf("Process 1 Exiting: Process 2 has finished.\n");
-                break;
-            }
-            else if (counter % 3 == 0){
-                printf("Process 1 Cycle number: %d - %d is a multiple of 3\n", counter, counter); /* C */
+        while(shared->counter <= 500){
+            if (shared->counter % shared->multiple == 0){
+                printf("Process 1: Counter = %d - %d is a multiple of 3\n", shared->counter, shared->counter); /* C */
                 sleep(1);
             }
             else{
-                printf("Process 1 Cycle number: %d\n", counter);
+                printf("Process 1: Counter = %d\n", shared->counter);
                 sleep(1);
             }
-            counter++;
+            shared->counter++;
            
         }
-    return 0;
+        printf("Process 1 Exiting: value of counter is over 500.\n");
+        
+        wait(NULL);
+
+        // Detach and remove shared memory
+        munmap(shared, SIZE);
+        shm_unlink(name);
+
+        return 0;
     }
 }
--- process2.c
+++ process2.c
@@ -2,22 +2,58 @@
 #include <unistd.h>  
 #include <sys/types.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+struct sharedMemory {
+    int multiple; // the multiple number
+    int counter;  // shared counter
+};
 
 int main()
 {  
-    int counter = 0;
-    while(counter >= -500){
-        if (counter % 3 == 0){
-            printf("Process 2 Cycle number: %d - (%d) is a multiple of 3\n", counter, counter);
+   //size (in bytes) of shared memory object 
+   const int SIZE = 4096;  
+   //name of the shared memory object             
+   const char *name = "OS";      
+
+   //shared memory file descriptor
+   int shm_fd;
+   // pointer to shared memory obect                      
+   struct sharedMemory *shared;      
+
+   // create the shared memory object
+   shm_fd = shm_open(name, O_RDWR, 0666);
+
+   // memory map the shared memory object
+   shared = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    int printed = -1;
+    while(shared->counter <= 100){
+        if(shared->counter != printed){
+            printf("Process 2: Counter = %d, waiting for value of counter to exceed 100\n", shared->counter);
+            printed = shared->counter;
+            sleep(1);
+        }
+    }
+
+    while(shared->counter <= 500){
+        if (shared->counter % shared->multiple == 0){
+            printf("Process 2: Counter = %d - %d is a multiple of 3\n", shared->counter, shared->counter);
             sleep(1);
         }
         else{
-            printf("Process 2 Cycle number: %d\n", counter);
+            printf("Process 2: Counter = %d\n", shared->counter);
             sleep(1);
         }
-        counter--;
-
     }
-    printf("Process 2 Exiting: reached a value lower than -500\n");
+    printf("Process 2 Exiting: value of counter is over 500\n");
+
+    munmap(shared, SIZE);
+    
+
     return 0;
 }
